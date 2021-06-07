3856d553f8dbdaa494bb5439d0929106d4a40ba5
Mark Lindeman <m.lindeman@picturae.com>
2021-06-07 10:43:00 +0200

    Use dynamic URL's for manifests in viewer

--- src/main/kotlin/nu/lindeman/iiif/Main.kt
+++ src/main/kotlin/nu/lindeman/iiif/Main.kt
@@ -17,6 +17,7 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
+import io.ktor.server.engine.*
 import io.ktor.util.*
 import io.ktor.utils.io.*
 import kotlinx.serialization.json.Json
@@ -26,6 +27,12 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     val config: Config = ConfigFactory.load()
     val nuxeo = Nuxeo(config.getConfig("app.nuxeo"))
+
+    var serverAddressUrl: URLBuilder
+
+    (environment as ApplicationEngineEnvironment).connectors[0].let{
+        serverAddressUrl = URLBuilder("${it.type.name}://${it.host}:${it.port.toString()}")
+    }
 
     install(AutoHeadResponse)
     install(CORS) {
@@ -49,7 +56,7 @@ fun Application.module() {
         manifest(nuxeo, config.getConfig("app.iiif"))
         document(nuxeo)
         documents(nuxeo)
-        viewer()
+        viewer(serverAddressUrl)
     }
 
 }
@@ -147,21 +154,21 @@ fun Route.cantaloupeReverseProxy(config: Config) {
     }
 }
 
-fun Route.viewer()
+fun Route.viewer(url: URLBuilder)
 {
     data class User(val id: Int, val name: String)
     get("/viewer/folder") {
-        val manifest = "http://localhost:8080/manifest/from/folder/by/uid/c7065ef6-ad5a-4235-8f05-1d179bc1e147"
+        val manifest = "${url.buildString()}/manifest/from/folder/by/uid/c7065ef6-ad5a-4235-8f05-1d179bc1e147"
         call.respond(FreeMarkerContent("mirador.ftl", mapOf("manifest" to manifest)))
     }
 
     get("/viewer/folder/{uuid}") {
-        val manifest = "http://localhost:8080/manifest/from/folder/by/uid/${call.parameters["uuid"]}"
+        val manifest = "${url.buildString()}/manifest/from/folder/by/uid/${call.parameters["uuid"]}"
         call.respond(FreeMarkerContent("mirador.ftl", mapOf("manifest" to manifest)))
     }
 
     get ("/viewer/folder/path/{...}") {
-        val manifest = "http://localhost:8080${call.request.uri.replace("/viewer/folder/path", "/manifest/from/folder/by/path")}"
+        val manifest = "${url.buildString()}${call.request.uri.replace("/viewer/folder/path", "/manifest/from/folder/by/path")}"
         println(manifest)
         call.respond(FreeMarkerContent("mirador.ftl", mapOf("manifest" to manifest)))
     }
